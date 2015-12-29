4fbb574deb0dea2ad48ec4688e7aca3bad5412f1
BanbanAndroid <1262098118@qq.com>
2015-12-29 11:47:42 +0800

    commit service change

--- src/main/java/com/place/helper/service/impl/PlacePoiidServiceImpl.java
+++ src/main/java/com/place/helper/service/impl/PlacePoiidServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.place.helper.persistent.entity.PlacePoiid;
 import com.place.helper.persistent.entity.PlacePois;
@@ -44,6 +45,26 @@ public class PlacePoiidServiceImpl implements PlacePoiidService {
 				PlacePoiid placePoiid = new PlacePoiid();
 				placePoiid.setPoiId(poiid);
 				placePoiid.setPlacePois(placePois);
+				String placeName = null;
+				String country = null;
+				String province = null;
+				String city = null;
+				String county = null;
+				try{
+					placeName = placeBlock.getString("title");
+					JSONObject district_info = placeBlock.getJSONObject("district_info");
+					country = district_info.getString("country");
+					province = district_info.getString("province");
+					city = district_info.getString("city");
+					county = district_info.getString("county");	
+				}catch(JSONException e){
+					e.printStackTrace();
+				}
+				placePoiid.setPlaceName(placeName);
+				placePoiid.setCity(city);
+				placePoiid.setCountry(country);
+				placePoiid.setCounty(county);
+				placePoiid.setProvince(province);
 				
 				if (placePoiid != null) {
 					poiidRepository.save(placePoiid);
